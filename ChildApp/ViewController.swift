ff58670fc9f72b756169ab16571b0ed3ad6e7afd
saurabhverma <vermasaura@gmail.com>
2017-07-19 15:48:40 +1000

    Initial commit

---
+++ ChildApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChildApp
+//
+//  Created by Saurabh Verma on 19/7/17.
+//  Copyright © 2017 Outware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
